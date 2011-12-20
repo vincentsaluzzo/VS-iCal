0dec1f96f2c28511a4076882453bc39ae7be4b8a
Vincent Saluzzo <vincentsaluzzo@gmail.com>
2011-12-20 10:40:44 +0100

    Initial Commit

---
+++ iCal-Facade-Library/iCal_Facade_Library.h
@@ -0,0 +1,13 @@
+//
+//  iCal_Facade_Library.h
+//  iCal-Facade-Library
+//
+//  Created by Vincent Saluzzo on 20/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iCal_Facade_Library : NSObject
+
+@end
